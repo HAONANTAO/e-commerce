da374dba273cdde6455d82c2bf92d3418920c680
HAONANTAO <873190199@qq.com>
2024-05-27 17:37:57 +1000

    the images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,88 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
-
+import { Check } from 'lucide-react';
 export default function Home() {
   return (
     <>
+
     <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-            <div className="col=span-2 px=6 lg:px-0 lg:pt-4">
-              <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-                <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                  <img src="/snake-1.png"/>
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                {/* <img src="/snake-1.png" alt="" className="w-full"/> */}
+                <Image
+                  src="/snake-1.png"
+                  alt=""
+                  className="w-full"
+                  // in difference screen can display
+                    layout="responsive"
+                    width={500}
+                      height={500}
+                    />
+
+              </div>
+              {/* line space and line height */}
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a  <span className="bg-green-600 px-2 text-white">
+                  Custom </span> Phone Case
+              </h1>
+              {/* max-w-prose--> keep the read friendly 65characters */}
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,<span className="font-semibold">one-of-one</span>Phone case.
+                CaseCobra allows you to protect your memories, not just your phone case.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  {/* dot points */}
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    Hight-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    5 years print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              
+              </ul>
+
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                {/* negative x space   */}
+                <div className="flex -space-x-4 ">
+                  {/* user images */}
+                   <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user1"
+                    width={40}
+                    height={40}
+                    />
+                 <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user2"
+                    width={40}
+                    height={40}
+                    />
+                 
+                    <Image
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user3"
+                    width={40}
+                    height={40}
+                    />
+
                 </div>
               </div>
             </div>
-
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
