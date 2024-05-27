ab6de38e7ead00728dd823cba47e2e053e3b13e1
HAONANTAO <873190199@qq.com>
2024-05-27 16:33:42 +1000

    copy public images

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+type MaxWidthWrapperProps = {  className?: string,
+  children?: React.ReactNode}
+
+const MaxWidthWrapper = ({className,children}:MaxWidthWrapperProps) => {
+  return (
+    // combine the classname
+    <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",className)}>{children}</div>
+  )
+}
+
+export default MaxWidthWrapper
