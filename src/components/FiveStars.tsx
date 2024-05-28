922f702c5a8091f5590624885abbc4bd9f1e42bb
HAONANTAO <873190199@qq.com>
2024-05-28 21:51:58 +1000

    kinde auth install

--- src/components/FiveStars.tsx
+++ src/components/FiveStars.tsx
@@ -3,12 +3,12 @@ import { Star } from 'lucide-react';
 const FiveStars = () => {
   return (
   <div className="flex gap-0.5">
-                    {/* filled */}
-                      <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-      <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-      <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-      <Star className="h-4 w-4 text-green-600 fill-green-600"/>
-      <Star className="h-4 w-4 text-green-600 fill-green-600"/>
+     {/* filled */}
+       <Star className="h-4 w-4 text-orange-600 fill-orange-600"/>
+      <Star className="h-4 w-4 text-orange-600 fill-orange-600"/>
+      <Star className="h-4 w-4 text-orange-600 fill-orange-600"/>
+      <Star className="h-4 w-4 text-orange-600 fill-orange-600"/>
+      <Star className="h-4 w-4 text-orange-600 fill-orange-600"/>
                   </div>
   )
 }
