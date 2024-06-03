57e9e47fea3eafbf0d0dd3d7109e2cc26d44dd2c
HAONANTAO <873190199@qq.com>
2024-06-03 16:51:16 +1000

    the underline of customer display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import FiveStars from "@/components/FiveStars";
 import UserImages from "@/components/UserImages";
 import Dotpoints from "@/components/Dotpoints";
 import Phone from "@/components/Phone";
+import {Icons} from "@/components/Icons";
 export default function Home() {
   return (
     <>
@@ -69,7 +70,10 @@ export default function Home() {
        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
         <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
           <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
-            What our <span className="relative px-2">customers</span> say?</h2>
+            What our
+             <span className="relative px-2">
+              customers<Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500"/>
+              </span> say?</h2>
           </div> 
         </MaxWidthWrapper>
       </section>
