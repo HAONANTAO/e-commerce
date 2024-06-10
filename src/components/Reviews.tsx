a07aa652a231e23dba3dab1eac6e669bdf886216
HAONANTAO <873190199@qq.com>
2024-06-10 19:08:10 +1000

    tmp save

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,7 +1,8 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { HTMLAttributes, useEffect, useRef, useState } from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import {useInView} from 'framer-motion'
+import { cn } from '@/lib/utils'
 
 const PHONES = [
   "/testimonials/1.jpg",
@@ -11,6 +12,44 @@ const PHONES = [
       "/testimonials/5.jpg",
        "/testimonials/6.jpg"
 ]
+
+function ReviewColumn({
+    reviews,className,reviewClassName,msPerPixel=0
+}:{
+  reviews: string[],
+  className?: string,
+  reviewClassName?:(reviewIndex: number)=> string,
+  msPerPixel?: number
+}){
+  const columnRef = useRef<HTMLDivElement | null>(null)
+  const [columnHeight,setColumnHeight] =useState(0) 
+  const duration =`${columnHeight * msPerPixel}ms`
+
+  useEffect(()=>{
+    if(!columnRef.current) return
+
+    const resizeObserver = new window.ResizeObserver(()=>{
+      setColumnHeight(columnRef.current?.offsetHeight?? 0)
+    })
+
+    resizeObserver.observe(columnRef.current)
+
+    return ()=>{
+      resizeObserver.disconnect()
+    }
+  },[])
+  return <div ref={columnRef} className={cn("animate-marquee space-y-8 py-4",className)} style={{"--marquee-duration": duration} as React.CSSProperties}>
+    {reviews.concat(reviews).map(imgSrc,reviewIndex)=>(
+      <Review/>
+    )}
+  </div>
+}
+interface ReviewProps extends HTMLAttributes<HTMLDivElement>{
+  imgSrc: string
+}
+function Review({imgSrc,className,...props}:ReviewProps){
+  return <div className={cn("animate-fade-in rounded-[2.25rem] bg-white  p-6 opacity-0 shadow-xl shadow-slate-900/5",className)} {...props}> </div>
+}
 // <T>泛型 代表任何
 function splitArray<T>(array: Array<T>, numParts:number){
     const result: Array<Array<T>> = [];
@@ -27,9 +66,19 @@ function splitArray<T>(array: Array<T>, numParts:number){
 function ReviewGrid(){
   const containerRef = useRef<HTMLDivElement>(null)
   const isInView = useInView(containerRef,{once:true,amount:0.4})
-
+  // 3 columns
   const columns = splitArray(PHONES,3)
-  return <div ref={containerRef} className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'></div>
+  const column1 = columns[0]
+  const column2 = columns[1]
+  const column3 = splitArray(columns[2],2)
+
+
+  return <div ref={containerRef} className='relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3'>
+    {isInView? <>
+    <ReviewColumn reviews={[]}/>
+    </>:null}
+
+  </div>
 }
 const Reviews = () => {
   return (
